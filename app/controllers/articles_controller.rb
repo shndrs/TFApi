da49df6d40ddf5f356d32d7ce6d2810f419f605b
Sahand Raeisi <sahandraeisi1994@gmail.com>
2019-09-12 10:46:40 +0430

    Updated index method in ArticlesController

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,10 @@
 
 class ArticlesController < ApplicationController
     def index
-        
+        articles = Article.all
+        render json: articles
     end
-end
+
+    def show
+    end
+end 
