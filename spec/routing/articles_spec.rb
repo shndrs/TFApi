8cc1b417e0e68c2aa799210253d80665e74b1aac
Sahand Raeisi <sahandraeisi1994@gmail.com>
2019-09-12 10:48:04 +0430

    Updated articles routes with show test

--- spec/routing/articles_spec.rb
+++ spec/routing/articles_spec.rb
@@ -4,4 +4,8 @@ describe 'articles routes' do
     it 'should rout to articles index' do
         expect(get '/articles').to route_to('articles#index')
     end
+
+    it 'should rout to articles show' do
+        expect(get '/articles/1').to route_to('articles#show', id: '1')
+    end
 end
