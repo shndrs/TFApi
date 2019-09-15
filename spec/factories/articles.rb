ebec3c1ab5194042a77f0af32e86f260f35ef211
Sahand Raeisi <sahandraeisi1994@gmail.com>
2019-09-15 09:10:36 +0430

    Updated Article FactoryBot with dynamic data

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :article do
-    title { "MyString" }
-    content { "MyText" }
-    slug { "MyString" }
+    sequence(:title) { |n| "My article #{n}" }
+    sequence(:content) { |n| "The content of my article #{n}" }
+    sequence(:slug) { |n| "my-article-#{n}" }
   end
 end
