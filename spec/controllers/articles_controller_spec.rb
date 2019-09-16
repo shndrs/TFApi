3661a956a6218c0019eb6c0d9a265a67d7c157c9
Sahand Raeisi <sahandraeisi1994@gmail.com>
2019-09-16 09:46:34 +0430

    dynamicalize the test cases

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -2,29 +2,26 @@ require 'rails_helper'
 
 describe ArticlesController do
     describe '#index' do
+
+        subject { get :index }
+
         it 'should return success response' do
-            get :index
+            subject
             expect(response).to have_http_status(:ok)
         end
 
         it 'should return proper json' do
-            create_list :article, 2
-            get :index
-            json = JSON.parse(response.body)
-            pp json
-            json_data = json['data']
-
+            articles = create_list :article, 2
+            subject
             expect(json_data.length).to eq(2)
-            expect(json_data[0]['attributes']).to eq({
-                "title" => "My article 1",
-                "content" => "The content of my article 1",
-                "slug" => "my-article-1"
-            })
-            expect(json_data[1]['attributes']).to eq({
-                "title" => "My article 2",
-                "content" => "The content of my article 2",
-                "slug" => "my-article-2"
-            })
+
+            articles.each_with_index do |article, index|
+                expect(json_data[index]['attributes']).to eq({
+                    "title" => article.title,
+                    "content" => article.content,
+                    "slug" => article.slug
+                })
+            end
         end
     end
 end
