1b7ea2e9c95dd1fba4d79a55fdadf9c85ac8ff37
Sahand Raeisi <sahandraeisi1994@gmail.com>
2019-09-16 15:10:46 +0430

    Added articles order tast case

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -23,5 +23,13 @@ describe ArticlesController do
                 })
             end
         end
+        it 'should return articles in the proper order' do
+            old_article = create :article
+            newer_article = create :article
+
+            subject
+            expect(json_data.first['id']).to eq(newer_article.id.to_s)
+            expect(json_data.last['id']).to eq(old_article.id.to_s)
+       end
     end
 end
