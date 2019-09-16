3b9316cdc67a1f97dfd864fad7c30a29b70aae84
Sahand Raeisi <sahandraeisi1994@gmail.com>
2019-09-16 15:11:34 +0430

    Added .recent describe

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -30,6 +30,24 @@ RSpec.describe Article, type: :model do
       invalid_article = build :article, slug: article.slug
       expect(invalid_article).not_to be_valid
     end  
+
+  describe '.recent' do
+    it 'should list recent article first' do
+
+      old_article = create :article
+      newer_article = create :article
+
+      expect(described_class.recent).to eq(
+        [ newer_article, old_article ]
+      )
+      
+      old_article.update_column :created_at, Time.now
+
+      expect(described_class.recent).to eq(
+        [ newer_article, old_article ]
+      )
+
+    end
   end
 end
  
